2ec48316b781b7b680940920a1fa3fbcb532aef7
Mahadi Hasan <mahadih137@gmail.com>
2023-10-20 00:30:44 +0600

    cart item delete api created

--- index.js
+++ index.js
@@ -124,6 +124,13 @@ async function run() {
       );
     });
 
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartItems.deleteOne(filter);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
